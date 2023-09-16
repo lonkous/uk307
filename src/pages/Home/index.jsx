58df611364a56b5adc749b50956afe96bae31d14
lonkous <github@kunoxy.ch>
2023-09-16 14:33:56 +0200

    test

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,9 +2,69 @@ import preactLogo from "../../assets/preact.svg";
 import "tailwindcss/tailwind.css";
 
 export function Home() {
+  let [isOpen, setIsOpen] = useState(true);
+  let [username, setUsername] = useState("");
+  let [password, setPassword] = useState("");
+
+  async function SignIn(username, password) {
+    try {
+      const response = await fetch(
+        "https://campus.csbe.ch/sollberger-manuel/uek307/Authenticate",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            username: username,
+            password: password,
+          }),
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  function clicked() {
+    SignIn(username, password);
+  }
+
+  function handleUserChange(event) {
+    setUsername(event.target.value);
+  }
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+  async function getCategory() {
+    const response = await fetch(
+      "https://campus.csbe.ch/sollberger-manuel/uek307/Products",
+      {
+        method: "GET",
+      }
+    );
+    console.log(response);
+  }
+
   return (
-    <div class="home">
-      <h1>Get Started building Vite-powered Preact Apps </h1>
+    <div class="flex items-center justify-center h-screen">
+      <div class="flex flex-col border-2 rounded-md p-2 sshadow-xl gap-2">
+        <h1 class="text-2xl p-2">Sign In</h1>
+        <input
+          type="text"
+          placeholder="root"
+          value={username}
+          onInput={handleUserChange}
+          class="rounded-md p-1 m-1"
+        ></input>
+
+        <input
+          type="text"
+          placeholder="Password"
+          value={password}
+          onInput={handlePasswordChange}
+          class="rounded-md p-1 m-1"
+        ></input>
+        <button onClick={clicked} class="rounded-md p-1 m-1 bg-violet-700">
+          Sign In
+        </button>
+      </div>
     </div>
   );
 }
