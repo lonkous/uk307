7712fc3a40dec9c251b905b0f64c391f3d8f1d11
lonkous <github@kunoxy.ch>
2023-09-23 12:48:23 +0200

    Impl: intial Categories

--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -1,8 +1,160 @@
 import { h } from "preact";
 import { useState, useEffect } from "react";
+import { Row } from "./row";
 
 export function Category() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isCreate, setIsCreate] = useState(false);
+
+  const [active, setActive] = useState("");
+  const [idCategory, setIdCategory] = useState("");
+  const [name, setName] = useState("");
+  const [refetch, setrefetch] = useState(false);
+
+  useEffect(() => {
+    async function getCategory() {
+      try {
+        const response = await fetch(
+          "https://campus.csbe.ch/sollberger-manuel/uek307/Categories?itsy-bitsy-teenie-weenie-yellow-polkadot-bikini",
+          {
+            method: "GET",
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const responseData = await response.json();
+        setData(responseData);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      }
+    }
+
+    getCategory();
+  }, [refetch]);
+
+  async function CreateItem() {
+    try {
+      const url =
+        "https://campus.csbe.ch/sollberger-manuel/uek307/category/" +
+        idCategory +
+        "?itsy-bitsy-teenie-weenie-yellow-polkadot-bikini";
+      console.log(url);
+
+      const response = await fetch(url, {
+        method: "PUT",
+        body: JSON.stringify({
+          active: active,
+          name: name,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const responseData = await response.json();
+      console.log("Data updated successfully:", responseData);
+    } catch (error) {
+      console.error("Error updating data:", error);
+    }
+  }
+
+  const handleUpdate = () => {
+    alert("Item Created!");
+    setrefetch(!refetch);
+    CreateItem();
+  };
+
   return (
-    <div class="min-h-screen flex items-center justify-center  m-5">lh3</div>
+    <div class="min-h-screen flex items-center justify-center m-5">
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && !data && <p>Error: Unable to fetch data</p>}
+      {!isLoading && data && (
+        <div class="flex flex-col gap-5">
+          <div class="rounded-lg w-full flex">
+            <button
+              class="bg-white text-neutral-900 capitalize rounded-full decoration-2 p-2 w-10 h-10 font-bold mr-2"
+              onClick={() => setIsCreate(!isCreate)}
+            >
+              +
+            </button>
+            <input
+              type="text"
+              class="p-2 pl-8 rounded-l-lg w-fill flex-grow ring-0 ring-white"
+              placeholder="Searching..."
+            />
+            <button class="bg-violet-500 text-neutral-100 underline underline-offset-2 capitalize decoration-2  p-2 rounded-r-lg w-1/5 font-bold">
+              search
+            </button>
+          </div>
+          <table class="bg-violet-500 rounded-lg ">
+            <tr class="bg-gray-300 shadow-2xl ">
+              <th class="p-2 w-fit text-neutral-900 hover:underline">
+                Edit Category
+              </th>
+              <th class="p-2 w-fit text-neutral-900 hover:underline">Active</th>
+              <th class="p-2 w-fit text-neutral-900 hover:underline">
+                Category ID
+              </th>
+              <th class="p-2 w-fit text-neutral-900 hover:underline">Name</th>
+
+              <th class="p-2 w-fit text-neutral-900 hover:underline">Delete</th>
+            </tr>
+
+            {isCreate && (
+              <tr class="bg-gray-500">
+                <td></td>
+
+                <td class="p-2 w-fit  hover:underline">
+                  <input
+                    class="p-2 rounded-lg bg-white w-full text-neutral-900"
+                    placeholder="Active"
+                    value={active}
+                    onChange={(e) => setActive(e.target.value)}
+                  ></input>
+                </td>
+                <td class="p-2 w-fit  hover:underline">
+                  <input
+                    class="p-2 rounded-lg bg-white w-full text-neutral-900"
+                    placeholder="Category ID"
+                    value={idCategory}
+                    onChange={(e) => setIdCategory(e.target.value)}
+                  ></input>
+                </td>
+                <td class="p-2 w-fit  hover:underline">
+                  <input
+                    class="p-2 rounded-lg bg-white w-full text-neutral-900"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  ></input>
+                </td>
+
+                <td class="p-2 w-fit text-neutral-900 hover:underline text-center">
+                  <button
+                    class="px-4 py-2 bg-green-500 text-white rounded-2xl hover:bg-green-800"
+                    onClick={() => handleUpdate()}
+                  >
+                    Create
+                  </button>
+                </td>
+              </tr>
+            )}
+
+            {data.map((item) => (
+              <Row {...item} />
+            ))}
+          </table>
+        </div>
+      )}
+    </div>
   );
 }
---
+++ src/pages/Category/row.jsx
@@ -0,0 +1,149 @@
+import "tailwindcss/tailwind.css";
+import { useState } from "preact/hooks";
+
+export function Row(item) {
+  let [isEditable, setEditable] = useState(false);
+  const handleDelete = (item) => {
+    const confirmation = window.confirm("Are you sure you want to delete?");
+    if (confirmation) {
+      alert("Item deleted!");
+      DeleteItem(item);
+    }
+  };
+  async function DeleteItem(item) {
+    {
+      try {
+        const response = await fetch(
+          "https://campus.csbe.ch/sollberger-manuel/uek307/Product/" +
+            item +
+            "?itsy-bitsy-teenie-weenie-yellow-polkadot-bikini",
+          {
+            method: "DELETE",
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const responseData = await response.json();
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+  }
+  async function UpdateItem(category_id) {
+    try {
+      const response = await fetch(
+        "https://campus.csbe.ch/sollberger-manuel/uek307/Product/" +
+          category_id +
+          "?itsy-bitsy-teenie-weenie-yellow-polkadot-bikini",
+        {
+          method: "UPDATE",
+          //fix body
+          body: JSON.stringify({
+            active: 1,
+            name: "Nice Category",
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const responseData = await response.json();
+      console.log("Data updated successfully:", responseData);
+    } catch (error) {
+      console.error("Error updating data:", error);
+    }
+  }
+  const handleEdit = () => {
+    setEditable(!isEditable);
+  };
+
+  const handleUpdate = (category_id) => {
+    setEditable(!isEditable);
+    UpdateItem(category_id);
+  };
+  return (
+    <>
+      <tr
+        key={item.category_id + item.name}
+        class=" hover:bg-violet-300 hover:border-1 hover:delay-175 hover:ease-linear hover:duration-300 hover:shadow-2xl hover:drop-shadow-2xl  "
+      >
+        <td>
+          <button
+            class="px-4 py-2 bg-red-500 text-white rounded-2xl hover:bg-red-800"
+            onClick={() => handleEdit()}
+          >
+            Edit
+          </button>
+        </td>
+        <td class="p-2 w-fit text-neutral-900 hover:underline align-center">
+          {item.active == 1 && (
+            <svg
+              class="align-middle  h-4 rounded-lg text-green-500"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="8"
+                d="M5.5 12.5l3 3 6-6"
+              />
+            </svg>
+          )}
+        </td>
+        <td class="p-2 w-fit text-neutral-900 hover:underline">
+          {item.category_id}
+        </td>
+        <td class="p-2 w-fit text-neutral-900 hover:underline">{item.name}</td>
+        <td class="p-2 w-fit text-neutral-900 hover:underline text-center">
+          <button
+            class="px-4 py-2 bg-red-500 text-white rounded-2xl hover:bg-green-800"
+            onClick={() => handleDelete(item.category_id)}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+      {isEditable && (
+        <tr
+          key={item.product_id}
+          class=" bg-white hover:border-1 hover:delay-175 hover:ease-linear hover:duration-300 hover:shadow-2xl hover:drop-shadow-2xl  "
+        >
+          <td>
+            <button
+              class="px-4 py-2 bg-teal-500 text-white rounded-2xl hover:bg-teal-800"
+              onClick={() => handleUpdate(item.category_id)}
+            >
+              Update
+            </button>
+          </td>
+          <td class="p-2 w-fit  hover:underline">
+            <input
+              class="p-2 rounded-lg bg-white w-full text-neutral-900"
+              placeholder="Active"
+            ></input>
+          </td>
+          <td class="p-2 w-fit  hover:underline">
+            <input
+              class="p-2 rounded-lg bg-white w-full text-neutral-900"
+              placeholder="Category ID"
+            ></input>
+          </td>
+          <td class="p-2 w-fit  hover:underline">
+            <input
+              class="p-2 rounded-lg bg-white w-full text-neutral-900"
+              placeholder="Name"
+              id="sku"
+            ></input>
+          </td>
+          <td class="p-2 w-fit  hover:underline"></td>
+        </tr>
+      )}
+    </>
+  );
+}
